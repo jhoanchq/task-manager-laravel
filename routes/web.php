862d5682478b9292566b2e6bba0511ee0364d84c
jhoanchq <jhoan.chiteq@gmail.com>
2026-06-12 09:11:31 -0500

    fix: add profile.edit route, fix tasks routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,10 @@ Route::get('/', function () {
 Route::middleware(['auth'])->group(function () {
     Route::redirect('/dashboard', '/tasks')->name('dashboard');
     Route::resource('tasks', TaskController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
